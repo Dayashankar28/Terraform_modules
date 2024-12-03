7e0ee365b9aa1803612c3cf8685c9bcd0b10e006
Dayashankar28 <dayamm28@gmail.com>
2024-12-03 19:40:21 +0000

    sdf

--- provisioners/output.tf
+++ provisioners/output.tf
@@ -5,3 +5,15 @@ output "IGW_NAME" {
 output "VPC_NAME" {
   value = module.create_vpc.vpc_name
 }
+
+output "CIDR_BLOCK" {
+  value = module.create_vpc.cidr_block
+}
+
+output "Public_Subnet" {
+  value = module.create_vpc.public_subnet_cidr
+}
+
+output "Private_Subnet" {
+  value = module.create_vpc.private_subnet_cidr
+}
