0e8dea541aba8bff4602384d5efd406c19a7f3ba
Dayashankar28 <dayamm28@gmail.com>
2024-12-03 10:10:41 +0000

    sda

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -4,12 +4,12 @@ module "create_ec2" {
   key_name = var.root_key_name
 }
 
-# module "remote-exec" {
-#   source = "github.com/Dayashankar28/terraform-modules-daya//modules/remote-exec"
-#   ec2_user = var.root_ec2_user
-#   ec2_public_ip = module.create_ec2.ec2_public_ip
-#   ec2_pem = var.root_key_path
-# }  
+module "remote-exec" {
+  source = "github.com/Dayashankar28/terraform-modules-daya//modules/remote-exec"
+  ec2_user = var.root_ec2_user
+  ec2_public_ip = module.create_ec2.ec2_public_ip
+  ec2_pem = var.root_key_path
+}  
 
 module "file-exec" {
   source = "github.com/Dayashankar28/terraform-modules-daya//modules/file_provisioner"
